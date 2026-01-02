ced648a6ed0a787fa0ebc15b4077baabd991126e
dpalexandru <alexpoian91@gmail.com>
2026-01-02 16:51:39 +0100

    feat: implement alphabetical sorting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,19 @@ function Home() {
   // stati
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("")
+  const [sortOrder, setSortOrder] = useState("asc") 
 
   // recupero i dati
   const { products, error } = useContext(ProductContext);
   // articoli filtrati
  const filtered = products
   .filter((p) => p.title.toLowerCase().includes(search.toLowerCase()))
-  .filter((p) => (category ? p.category === category : true));
+  .filter((p) => (category ? p.category === category : true))
+  .sort((a, b) => {
+    return sortOrder === "asc" 
+      ? a.title.localeCompare(b.title) 
+      : b.title.localeCompare(a.title);
+  });
 
   return (
     <>
@@ -56,6 +62,18 @@ function Home() {
               className="p-2 border rounded-lg w-full max-w-md shadow-sm focus:ring-2 focus:ring-indigo-500 outline-none"
               onChange={(e) => setSearch(e.target.value)}
             />
+          </div>
+
+
+
+          <div className="w-full md:w-40">
+            <select 
+              className="w-full p-2.5 border border-slate-200 rounded-lg bg-white shadow-sm outline-none focus:ring-2 focus:ring-indigo-500 cursor-pointer text-slate-700 transition-all"
+              onChange={(e) => setSortOrder(e.target.value)}
+            >
+              <option value="asc">Title: A to Z</option>
+              <option value="desc">Title: Z to A</option>
+            </select>
           </div>
 
           </div>
